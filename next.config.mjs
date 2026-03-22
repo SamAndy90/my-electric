ea8808bbab605cb11f7442dcb16c2d2875b4fa3f
Andrii Stadnyk <stadnyk.andy@gmail.com>
2026-03-23 00:57:18 +0200

    first review fix config

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,10 @@ const nextConfig = {
         protocol: 'http',
         hostname: 'localhost',
       },
+      {
+        protocol: 'https',
+        hostname: 'my-electric.vercel.app',
+      },
     ],
   },
 
